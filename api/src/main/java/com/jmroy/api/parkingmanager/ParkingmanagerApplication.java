ea6c986757edaebcb6d9cc4f1da67d5d8fb1be33
Jean-Marie Roy <jeanmroy@gmail.com>
2025-03-06 14:11:53 -0500

    Parking Manager initial commit

---
+++ api/src/main/java/com/jmroy/api/parkingmanager/ParkingmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.jmroy.api.parkingmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingmanagerApplication.class, args);
+	}
+
+}
