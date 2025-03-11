47d52508411b9981479b875b2b59cad74f01eb0b
Jean-Marie Roy <jeanmroy@gmail.com>
2025-03-10 23:37:44 -0400

    Fix mapstruct issue

--- src/main/java/com/jmroy/api/parkingmanager/api/vehicule/VehiculeMapper.java
+++ src/main/java/com/jmroy/api/parkingmanager/api/vehicule/VehiculeMapper.java
@@ -10,9 +10,13 @@ public interface VehiculeMapper {
 
     @Mapping(source = "owner.id", target = "ownerId")
     @Mapping(source = "location.id", target = "locationId")
+    @Mapping(source = "color", target = "color")
+    @Mapping(source = "licencePlate", target = "licencePlate")
     VehiculeDTO toDto(Vehicule vehicule);
 
     @Mapping(source = "ownerId", target = "owner.id")
     @Mapping(source = "locationId", target = "location.id")
+    @Mapping(source = "color", target = "color")
+    @Mapping(source = "licencePlate", target = "licencePlate")
     Vehicule toEntity(VehiculeDTO vehiculeDTO);
 }
